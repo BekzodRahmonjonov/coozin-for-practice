f7dd7ec420a7e2df5ecf273884541607223acdd4
Sarkor2007 <abilkosimovsarkor@gmail.com>
2022-12-07 17:26:19 +0500

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import TheHeader from "@/components/TheHeader";
 import TheFooter from "@/components/TheFooter";
 import TheMain from "@/components/TheMain";
 
+
 export default {
   name: 'App',
   components: {
@@ -21,6 +22,14 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+}
 .container {
     margin: 0 auto;
     max-width: 1240px;
--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,47 @@
 <template>
-    <h1>Footer</h1>
+    <footer>
+        <div class="container">
+            <div class="footer">
+                <div class="footer__top">
+                    <div class="footer__logo">
+                        <img src="../assets/logo_coozin.png" alt="footer-logo">
+                    </div>
+                    <div class="footer__items">
+                        <div class="footer_nav">
+                            <a href="#" class="nav__item">Наша история</a>
+                            <a href="#" class="nav__item">Присоединиться</a>
+                            <a href="#" class="nav__item">Блог</a>
+                        </div>
+                        <div class="footer__pay">
+                            <h3>Безопаская оплата</h3>
+                            <div class="footer__pay-img">
+                                <img src="../assets/apelsin_logo.png" alt="apelsin-logo">
+                            </div>
+                        </div>
+                        <div class="footer__support">
+                            <h3>При поддержке</h3>
+                            <div class="support__images">
+                                <img src="../assets/cola_logo.png" alt="cola-logo">
+                                <img src="../assets/osce_logo.png" alt="osce-logo">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer__bottom">
+                    <div class="footer__lisence">© 2022 Coozin  All Rights Reserved</div>
+                    <div class="footer__social">
+                        <p>ПОДПИСЫВАЙТЕСЬ НА НАС</p>
+                        <div class="social__line"></div>
+                        <div class="social__images">
+                            <a href=""><img src="../assets/insta.png" alt="insta"></a>
+                            <a href=""><img src="../assets/facebook.png" alt="facebook"></a>
+                            <a href=""><img src="../assets/YT.png" alt="youtube"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script>
@@ -9,7 +51,104 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-    color: blue;
+footer{
+    height: 362px;
+    border: 1px solid red;
+    padding: 50px 0px 30px;
 }
+
+.footer__top{
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+}
+
+.footer__items{
+    width: 100%;
+    max-width: 724px;
+    display: flex;
+    justify-content: space-around;
+}
+
+.footer__logo{
+    width: 148px;
+}
+
+.footer__logo>img{
+    width: 100%;
+}
+
+.nav__item{
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    color: #242C39;
+    text-decoration: none;
+    display: block;
+    margin-bottom: 20px;
+}
+
+.nav__item:hover{
+    color: red;
+}
+
+h3{
+    color: #374151;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 24px;
+    margin-bottom: 19px;
+}
+
+.support__images{
+    display: flex;
+    justify-content: space-between;
+    width: 210px;
+}
+
+.footer__bottom{
+    margin-top: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.footer_lisence{
+    font-weight: 300;
+    font-size: 16px;
+    line-height: 24px;
+    color: #1F2937;
+}
+.footer__social{
+    display: flex;
+    align-items: center;
+}
+
+.footer__social p{
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: 0.1em;
+    color: #1F2937;
+    position: relative;
+}
+
+.social__line{
+    width: 64px;
+    border: 1px solid #1F2937;
+    margin: 0px 16px;
+}
+
+.social__images a{
+    width: 40px;
+    height: 40px;
+    margin: 0px 10px;
+}
+
+.social__images{
+    display: flex;
+    align-content: center;
+}
+
 </style>
